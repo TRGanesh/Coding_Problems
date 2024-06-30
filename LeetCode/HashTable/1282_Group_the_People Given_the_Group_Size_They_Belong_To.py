7ee1297b9fa33a1e97ab0d89a6ac30128d8a5ba6
TRGanesh <117368449+TRGanesh@users.noreply.github.com>
2024-06-30 21:16:29 +0530

    Update 1282_Group_the_People Given_the_Group_Size_They_Belong_To.py

--- LeetCode/HashTable/1282_Group_the_People Given_the_Group_Size_They_Belong_To.py
+++ LeetCode/HashTable/1282_Group_the_People Given_the_Group_Size_They_Belong_To.py
@@ -43,6 +43,7 @@ Ex: groupSizes = [3,3,3,3,3,1,3]
     --> Now, we just have to break that List into parts,, where each part has size = GroupSize i.e 3
     --> [0,1,2],[3,4,6]
 '''
+# Method 1
 from collections import defaultdict
 class Solution:
     def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
@@ -88,3 +89,78 @@ class Solution:
                     numOfDivisions -= 1
 
         return groups
+
+# -------------------------------------------------------------------------------------------------------
+# Method 2
+# In above Method, Splitting of Sub-Groups is done with many Condition Checks & a While Loop & Using Pointers
+# We can also Simply find the Sub-Groups,, by Re-Slicing the Same List(Removing front "grpSize" numbered guys from list)
+
+from collections import defaultdict
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groupSize_ids_map = defaultdict(list)
+        
+        for idx, grpSize in enumerate(groupSizes):
+            groupSize_ids_map[grpSize].append(idx)
+        
+        groups = [] # Final Answer
+
+        for grpSize, ids in groupSize_ids_map.items():
+            while len(ids) >= grpSize:
+                sub_grp = ids[ 0 : grpSize ]
+                groups.append(sub_grp)
+
+                # Let's update the List ids,, such that we can remove the 1st "grpSize" numbered people using Slicing
+                ids = ids[ grpSize : ]
+
+        return groups    
+
+# -------------------------------------------------------------------------------------------------------
+# Method 3
+# In above 2 Methods, 1st we prepared the Map & then we performed the Splitting/Slicing Operations on Values of Map
+# Can we do the Same when Buiding the Map ??
+
+from collections import defaultdict
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groupSize_ids_map = defaultdict(list)
+        groups = [] # Final Answer
+
+        for idx, grpSize in enumerate(groupSizes):
+            groupSize_ids_map[grpSize].append(idx)
+
+            # Here,itself we can check the size of List(Value of Corresponding key) which is getting formed
+            if len(groupSize_ids_map[grpSize]) == grpSize:
+                groups.append(groupSize_ids_map[grpSize])
+
+                # So, we appended the List into Final Ans
+                # Then we have to Re-Initialize the Val of Corresponding Key to Empty List
+                groupSize_ids_map[grpSize] = []
+
+        return groups
+
+# Final Thoughts
+'''
+- In method 1,, We did the Basic Operations for Splitting / Dividing the List into Parts of equal-sized
+    - Those Operations includes,,
+        --> Properly Checking the Size of List & comparing with Desired_Size 
+        --> Finding the numOfDivisions, Using 2 Pointers for Slicing the ID's List(without Changing that List)
+- In method 2,, We thought like,, We have a List & We want to break it into parts such that each part should have a desired size
+    - So,, why don't we simply Do Re-Slicing the same List(Due to this List will update)
+    - Such that at 1st we take k guys from front & Then We Re-Slice the List such that updated list will start from remaining guys
+- In method 3,, We thought like,, Okay we are making the List(Value of Map) into parts,, whenever it's size equals to desiredSize
+    - So,, Instead of Calculating for entire Array, Can't we break the List whenever we are Updating the Map
+    - There Re-Initialization of Value is an important step
+'''
+            
+
+
+
+
+
+
+
+
+
+
+      
