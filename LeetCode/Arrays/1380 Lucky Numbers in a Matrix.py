f2d13753ad3c9378cb676977548f4405dbd2dd84
TRGanesh <117368449+TRGanesh@users.noreply.github.com>
2024-07-19 12:16:44 +0530

    Update 1380 Lucky Numbers in a Matrix.py

--- LeetCode/Arrays/1380 Lucky Numbers in a Matrix.py
+++ LeetCode/Arrays/1380 Lucky Numbers in a Matrix.py
@@ -52,3 +52,41 @@ class Solution:
                     ans.append(ele)
 
         return ans
+# ------------------------------------------------------------------------------
+
+'''
+- When we assumed 2 Lucky numbers and did some math
+    - We got a Contradiction such that there is no Possibilty of existance of 2 Lucky Numbers
+- Soo,, for all Cases there will be only a Single Lucky Number
+- Okay that guy will be exactly one of the member in minimum guys right!!! 
+    - That too,, the same guy is Max in col
+    - Means we have to select the Max of Min Row guys &&
+                       select the Min of Max Col guys 
+'''
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        num_rows = len(matrix)
+        num_cols = len(matrix[0])
+        
+        row_min = []
+        col_max = []
+        # Let's just store Min Guys for each row
+        for i in range(num_rows):
+            for j in range(num_cols):
+                row = matrix[i]
+                row_min.append(min(matrix[i]))
+        
+        for col in range(num_cols):
+            col_maxi = float('-inf')
+            for row in range(num_rows):
+                col_maxi = max(col_maxi, matrix[row][col])
+            
+            col_max.append(col_maxi)
+        
+        row_min_maxi = max(row_min)
+        col_max_mini = min(col_max)
+        
+        if row_min_maxi == col_max_mini:
+            return [row_min_maxi]
+        else:
+            return []
