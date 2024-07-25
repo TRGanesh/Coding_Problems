c67df33f86b7412091ba9586f5f4e74b8064adbb
TRGanesh <117368449+TRGanesh@users.noreply.github.com>
2024-07-25 23:36:32 +0530

    Create Flipping the Matrix.py

---
+++ Hackerrank 1 Month Prep Kit/Flipping the Matrix.py
@@ -0,0 +1,13 @@
+'''
+You are given a 2n x 2n matrix (a square matrix with even dimensions). 
+You are allowed to flip any of its rows or columns. 
+The goal is to maximize the sum of the elements in the upper-left quadrant of size n x n.
+'''
+
+'''
+- There Will be four parts for a n x n Matrix. 
+   - Upper-Left, Upper-Right, Lower-Left, and Lower-Right
+- Soo,, We want to Maximise the Upper-Left part
+- Okayy,, We can Reverse the Row or Column
+- We have to compare Upper-Left part elements with corresponding elements in other Quadrants
+'''
